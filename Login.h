78ab41026a08b45c8a9bd7cecb4dabc49e8e5095
Zimraan Anjum <zimraan2012@gmail.com>
2022-10-25 14:39:45 +1100

    creating local databse...sorta

--- Login.h
+++ Login.h
@@ -1,15 +1,29 @@
 #include <iostream>;
+#include <string>;
+#include <fstream>;
 using namespace std;
 
 class LoginSystem {
   public:
+  string username;
+  string password;
  
   int createNewUser();
   int returnExistingUser();
 };
 
 int LoginSystem::createNewUser(){
-  cout << "NEW USER";
+  std::cout << "Please enter your username.\n";
+  cin >> username;
+  std::cout << "Please enter in your password. \n";
+  cin >> password;
+
+  ofstream fout;
+  fout.open("data\\" + username + ".txt" );
+
+  fout << username << endl << password;
+
+  fout.close();
 
   return 0;
 }
